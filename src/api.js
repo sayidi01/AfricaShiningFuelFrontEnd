37f612c160a6c8a606ade5275af2b2bfbd6784cf
hicham sayidi <hicham13sayidi@gmail.com>
2024-05-22 11:17:28 +0100

    modif axios

--- src/api.js
+++ src/api.js
@@ -1,9 +1,16 @@
 import axios from "axios";
 import { isProd } from "./config";
 
+const accessControlAllowOrigin = isProd
+  ? "https://asf.ma"
+  : "http://localhost:5173";
+
 export const axiosInstance = axios.create({
   baseURL: isProd
     ? "https://africa-shining-fuel-server-1.onrender.com"
     : "http://localhost:3000",
   withCredentials: true,
+  headers: {
+    "Access-Control-Allow-Origin": accessControlAllowOrigin,
+  },
 });
