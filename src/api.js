67ec04f4c96df924759fb44d41f32d1e4c74a551
Zakaria Imzilen <zakariyaimzilen@gmail.com>
2024-05-23 12:02:22 +0100

    Fixing cross

--- src/api.js
+++ src/api.js
@@ -2,11 +2,10 @@ import axios from "axios";
 import { isProd } from "../src/config";
 
 const accessControlAllowOrigin = isProd == false ? "http://localhost:5173" : "https://asf.ma";
+const baseURL = isProd == false ? "http://localhost:3000" : "https://africa-shining-fuel-server-1.onrender.com";
 
 export const axiosInstance = axios.create({
-  baseURL: isProd
-    ? "https://africa-shining-fuel-server-1.onrender.com"
-    : "http://localhost:3000",
+  baseURL,
   withCredentials: true,
   headers: {
     "Access-Control-Allow-Origin": accessControlAllowOrigin,
