b6693bc1f570ab42b7e00918552811c630148c29
hicham sayidi <hicham13sayidi@gmail.com>
2024-06-19 10:06:59 +0100

    change api

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { isProd } from "../src/config";
 
 const accessControlAllowOrigin = !isProd ? "http://localhost:5173" : "https://asf.ma";
-const baseURL = !isProd ? "http://localhost:3000" : "https://africa-shining-fuel-server-79es.onrender.com";
+const baseURL = !isProd ? "http://localhost:3000" : "https://africa-shining-fuel-server-79es:3000";
 
 export const axiosInstance = axios.create({
   baseURL,
